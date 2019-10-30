fc6911a83a712acd18666e80f9507d1e163d543e
Lívio Silva <ifcontas32@gmail.com>
2019-10-30 16:16:42 -0300

    Testando

--- Js/index.js
+++ Js/index.js
@@ -4,7 +4,10 @@ function pegaCity (){
 
 function montaLink (){
     var comeco = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+
     var recebeCity = pegaCity ();
-    var final = '.json?access_token=pk.eyJ1IjoiY2ZsYmVkdWNhdG9yIiwiYSI6ImNrMTZrYm1vNTA1dWEzaGxqN2tmMTZlazcifQ.XXsWkpgiguegb-C7WQpGBA';
+
+    var final = '.json?access_token=pk.eyJ1IjoiY2ZsYmVkdWNhdG9yIiwiYSI6ImNrMTZrYm1vNTA1dWEzaGxqN2tmMTZlazcifQ.XXsWkpgiguegb-C7WQpGBA';   
+    
     return comeco + recebeCity + final;
 }
