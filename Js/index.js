1500ccd18c38173515d7e2fb94a809d75dac52f3
Lívio Silva <ifcontas32@gmail.com>
2019-10-30 15:19:23 -0300

    Primeiro commit

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+function pegaCity (){
+    return document.getElementById ('cidade').value;
+}
+
+function montaLink (){
+    var comeco = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+    var recebeCity = pegaCity ();
+
+    var final = '.json?access_token=pk.eyJ1IjoiY2ZsYmVkdWNhdG9yIiwiYSI6ImNrMTZrYm1vNTA1dWEzaGxqN2tmMTZlazcifQ.XXsWkpgiguegb-C7WQpGBA';
+    return comeco + recebeCity + final;
+}
